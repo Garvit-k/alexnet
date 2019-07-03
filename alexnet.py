26db99e84a5a2fc8163c19ba533514acca4f7528
Garvit Kothari <31393258+garvit-k@users.noreply.github.com>
2019-07-03 11:47:58 +0530

    max pool and dropout

--- alexnet.py
+++ alexnet.py
@@ -6,5 +6,26 @@ import tensorflow as tf
 import numpy as np
 
 # Convolutional Layer
-def convLayer(x, height, width, strideX, strideY, featureNum, name, padding ="SAME", groups =1):
+def convLayer(x, height, width, strideX, strideY,
+              featureNum, name, padding ="SAME", groups =1):
     channel = int(x.get_shape()[-1])
+
+# Max Pooling Layer
+def pool(x, height, width, strideX, strideY,
+         name, padding = "SAME"):
+    return tf.nn.max_pool(x, ksize = [1, height, width, 1],
+                          strides = [1, strideX, strideY, 1],
+                          padding = padding,
+                          name = name)
+
+# Response Normalization Layer
+def RNL(x, R, alpha, beta, name = None, bias = 1.0):
+    return tf.nn.local_response_normalization(x,
+                                              depth_radius = R,
+                                              alpha = alpha,
+                                              beta = beta,
+                                              bias = bias,
+                                              name = name)
+# dropout
+def dropout(x, keepPro, name = None):
+    return tf.nn.dropout(x, keepPro, name)
